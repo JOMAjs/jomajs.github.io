73f38c95328209b1d6611da33a436c09e8e9cff6
JEIZON ARIAS <46834146+JOMAjs@users.noreply.github.com>
2023-12-07 10:08:49 -0500

    Update button.js

--- views/templates/js/button.js
+++ views/templates/js/button.js
@@ -1,33 +1,56 @@
 $(function () {
 
     $(document).on("click", "#resume", function () {
-        console.log("hola como esta");
-        softwareApp.setLoadData({
+        $.ajax({
             url: "views/pages/resume.html",
             success: function (dataResume) {
-                $("#contenedor_cambiar_header").fadeIn(100).html(dataResume);
+                $("#contenedor_cambiar_header").html(dataResume);
+                $("#loader2").css("display", "block").slideUp(1000);
             }
         });
     })
 
     $(document).on("click", "#inicio", function () {
-        softwareApp.setLoadData({
+        $.ajax({
             url: "views/pages/inicio.html",
             success: function (dataInicio) {
                 $("#contenedor_cambiar_header").html(dataInicio);
-
+                $("#loader2").css("display", "block").slideUp(1000);
             }
         });
     })
 
     $(document).on("click", "#portafolio", function () {
-        softwareApp.setLoadData({
+        $.ajax({
             url: "views/pages/portafolio.html",
             success: function (dataPortafolio) {
                 $("#contenedor_cambiar_header").html(dataPortafolio);
+                $("#loader2").css("display", "block").slideUp(1000);
+
+            }
+        });
+    });
+
+    $(document).on("click", "#contacteme", function () {
+        $.ajax({
+            url: "views/pages/contacto.html",
+            success: function (dataPortafolio) {
+                $("#contenedor_cambiar_header").html(dataPortafolio);
+                $("#loader2").css("display", "block").slideUp(1000);
 
             }
         });
     })
+
+    document.onkeydown = function (e) {
+        if (e.ctrlKey &&
+            (e.keyCode === 85)) {
+            return false;
+        }
+    };
+
+    document.oncontextmenu = function () {
+        return false;
+    }
 
 })
