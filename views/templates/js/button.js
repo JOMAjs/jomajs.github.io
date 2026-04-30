2f8661ef1c94baa30af8b0c28dcdc3d087d76d6b
JEIZON ARIAS <46834146+JOMAjs@users.noreply.github.com>
2026-04-29 19:29:20 -0500

    Update button.js

--- views/templates/js/button.js
+++ views/templates/js/button.js
@@ -42,15 +42,40 @@ $(function () {
         });
     })
 
-    /*document.onkeydown = function (e) {
-        if (e.ctrlKey &&
-            (e.keyCode === 85)) {
-            return false;
-        }
-    };
+    // bloquear administrador de desarrollador
+        document.onkeydown = function(e) {
+            if (e.ctrlKey && (e.keyCode === 85)) {
+                return false;
+            }
+        };
 
-    document.oncontextmenu = function () {
-        return false;
-    }*/
+        document.oncontextmenu = function() {
+            return false;
+        } 
+
+        document.addEventListener('keydown', function(e) {
+             if (
+                 e.key === 'F12' ||
+                 (e.ctrlKey && e.shiftKey && ['I', 'C', 'J'].includes(e.key)) ||
+                 (e.ctrlKey && e.key === 'U')
+             ) {
+                 e.preventDefault();
+             }
+         });
+
+         window.addEventListener("load", () => {
+             setTimeout(() => window.scrollTo(0, 1), 100);
+         });
+
+         setInterval(function() {
+             const start = performance.now();
+             debugger;
+             const end = performance.now();
+
+             if (end - start > 100) {
+                 window.location.href = "error";
+             }
+         }, 1000);
+
 
 })
